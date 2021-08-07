e588bad0d87c5ae0fe28b65c2d509f942b5526a7
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-07 12:54:34 +0800

    added some features

--- bin/index.js
+++ bin/index.js
@@ -4,53 +4,68 @@
 
 // load the nodejs modules
 const fs = require("fs");
+const chalk = require("chalk");
+const figlet = require("figlet");
 const ps = require("prompt-sync");
 const prompt = ps();
 
-const getName = prompt("login ");
+console.log(
+  chalk.whiteBright(
+    figlet.textSync("dkatalis bank", {
+      horizontalLayout: "default",
+      verticalLayout: "default",
+    })
+  )
+);
 
-const data = {
-  name: getName,
-  balance: 0,
-};
+const getName = prompt("enter username : ");
 
-const saveData = (data) => {
-  const finished = (error) => {
-    if (error) {
-      console.error(error);
-
-      return;
-    }
+if (getName !== "") {
+  const data = {
+    name: getName,
+    balance: 0,
   };
-  const jsonData = JSON.stringify(data, null, 2);
 
-  fs.writeFile(`${data.name}.json`, jsonData, finished);
-};
+  const saveData = (data) => {
+    const finished = (error) => {
+      if (error) {
+        console.error(error);
 
-saveData(data);
+        return;
+      }
+    };
+    const jsonData = JSON.stringify(data, null, 2);
 
-// helper function to read data from json
-function jsonReader(filePath, cb) {
-  fs.readFile(filePath, "utf-8", (err, fileData) => {
+    fs.writeFile(`./data/${data.name}.json`, jsonData, finished);
+  };
+  saveData(data);
+
+  // helper function to read data from json
+  function jsonReader(filePath, cb) {
+    fs.readFile(filePath, "utf-8", (err, fileData) => {
+      if (err) {
+        return cb && cb(err);
+      }
+      try {
+        const obj = JSON.parse(fileData);
+
+        return cb && cb(null, obj);
+      } catch (err) {
+        return cb && cb(err);
+      }
+    });
+  }
+
+  // read data from json
+  jsonReader(`./data/${data.name}.json`, (err, data) => {
     if (err) {
-      return cb && cb(err);
-    }
-    try {
-      const obj = JSON.parse(fileData);
-
-      return cb && cb(null, obj);
-    } catch (err) {
-      return cb && cb(err);
+      console.log(err);
+    } else {
+      console.log(`Hello, ${data.name}`);
+      console.log(`Your balance is $${data.balance}`);
     }
   });
+} else {
+  console.log(`Sorry, you didn't enter username, please try again!!`);
+  process.exit(1);
 }
-
-// read data from json
-jsonReader(`./${data.name}.json`, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Hello, ${data.name}`);
-    console.log(`Your balance is $${data.balance}`);
-  }
-});
