6759c5ea1568445124e82577504c349caf5042e6
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-07 11:31:31 +0800

    make helper function to read data from json

--- bin/index.js
+++ bin/index.js
@@ -21,4 +21,27 @@ const saveData = (data) => {
 
 saveData(data);
 
-console.log(`my name is ${data.name} and my age is ${data.age}`);
+// helper function to read data from json
+function jsonReader(filePath, cb) {
+  fs.readFile(filePath, "utf-8", (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const obj = JSON.parse(fileData);
+
+      return cb && cb(null, obj);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+
+// read data from json
+jsonReader("./data.json", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`my name is ${data.name} and my age is ${data.age}`);
+  }
+});
